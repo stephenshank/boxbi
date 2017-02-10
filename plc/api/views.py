58aecca4230740bff83f8040f3ebbc2cf103c8e2
stephenshank <sshank314@gmail.com>
2017-02-10 04:53:21 -0500

    Prepares to test PLC communication.

--- plc/api/views.py
+++ plc/api/views.py
@@ -12,13 +12,12 @@ def plc(request):
     plcs = PLC.objects.filter(ip='db')
     ip_address = plcs[0].ip.address
     tags = {plc.plc_id: plc.tag for plc in plcs}
-    return JsonResponse(tags)
-    # c = SlcDriver()
-    # states = {}
-    # if c.open(ip_address):
-    #     for key, value in tags.iteritems():
-    #         try:
-    #             states[key] = c.read_tag(value)
-    #         except:
-    #             states[key] = ''
-    # return JsonResponse(states)
+    c = SlcDriver()
+    states = {}
+    if c.open(ip_address):
+        for key, value in tags.iteritems():
+            try:
+                states[key] = c.read_tag(value)
+            except:
+                states[key] = ''
+    return JsonResponse(states)
