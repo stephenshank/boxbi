80ce60878e57432ffccc113166bcea94c0a3137a
stephenshank <sshank314@gmail.com>
2017-02-10 05:53:43 -0500

    IP now displays properly in admin site.

--- plc/api/models.py
+++ plc/api/models.py
@@ -9,6 +9,9 @@ class IP(models.Model):
     description = models.CharField(max_length=64)
     address = models.CharField(max_length=16)
 
+    def __str__(self):
+        return self.ip_id
+
 
 class PLC(models.Model):
     plc_id = models.CharField(primary_key=True, max_length=32)
