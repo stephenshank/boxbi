76e8ecb85c43891e4fd6963e9abc6a9fc9f8799b
Stephen Shank <sshank314@gmail.com>
2017-02-12 18:18:12 -0500

    Initial attempt at communicating with ODBC data.

--- plc/api/views.py
+++ plc/api/views.py
@@ -1,5 +1,9 @@
+import json
+import os
+
 from django.http import JsonResponse
 from pycomm.ab_comm.slc import Driver as SlcDriver
+import pyodbc
 
 from api.models import PLC
 
@@ -21,3 +25,47 @@ def plc(request, ip):
             except:
                 states[key] = ''
     return JsonResponse(states)
+
+
+def odbc(request):
+    odbc_path = os.path.join('data', 'odbc.txt')
+    with open(odbc_path, 'r') as odbc_file:
+        connection_string = odbc_file.read()
+    connection = pyodbc.connect(connection_string)
+    cursor = connection.cursor()
+
+    order_query = '''SELECT o.QTY_ORDERED, c.CSNAME
+                     FROM ORDERS AS o
+                     JOIN CUSTOMER AS c
+                     ON o.CSCODE = c.CSCODE
+                     WHERE o.ORDER_NO=%s''' % request.GET['OrderNumber']
+    cursor.execute(order_query)
+    result = cursor.fetchone()
+    order_info = {
+        'QuantityOrdered': result[0],
+        'CustomeName': result[1]
+    }
+
+    roll_info = {}
+    number_of_rolls = request.GET['NumberOfRolls']
+    for i in range(int(number_of_rolls)):
+        roll_number = request.GET['RollNumber%d' % (i+1)]
+        roll_query = '''SELECT i.INVBITEMNO, i.ITEM_WEIGHT, i.RS_LF, v.VNNAME
+                        FROM INVENTORY_BIN as i
+                        JOIN VENDOR as v
+                        ON i.VNCODE = v.VNCODE
+                        WHERE i.INVBBINNO='ROLLSTOCK'
+                        AND i.RS_VENDOR_ROLL_NO='%s';''' % roll_number
+        cursor.execute(roll_query)
+        result = cursor.fetchone()
+        roll_info[roll_number] = {
+            'Info': result[0],
+            'Weight': float(result[1]),
+            'Lineal': float(result[2]),
+            'Vendor': result[3]
+        }
+
+    cursor.close()
+    connection.close()
+    all_info = {'Order': order_info, 'Rolls': roll_info}
+    return JsonResponse(all_info)
--- plc/plc/urls.py
+++ plc/plc/urls.py
@@ -22,5 +22,6 @@ from api import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^test/?', views.test),
-    url(r'^plc/(?P<ip>[a-z]+)/?', views.plc)
+    url(r'^plc/(?P<ip>[a-z]+)/?', views.plc),
+    url(r'^odbc', views.odbc)
 ]
